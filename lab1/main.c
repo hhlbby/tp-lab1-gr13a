2c3f73c04f2795deb81838268193d2ea20d1f5b7
Artem Patskov <artempatskov2007@gmail.com>
2026-02-15 21:35:28 +0300

    task 5 added

---
+++ lab1/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	double l;
+	double area;
+	printf("Введите длину окружности: ");
+	if (scanf("%lf", &l) != 1) {
+		printf("Ошибка: необходимо ввести число.\n");
+		return 1;
+	}
+        if (l <= 0) {
+       		 printf("Ошибка: длина окружности должна быть положительным числом.\n");
+       		 return 1;
+    	}
+	area = (l * l) / (4.0 * M_PI);
+	printf("Площадь круга: %.4lf\n", area);
+	return 0;
+
+}
